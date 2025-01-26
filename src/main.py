ea312da06565ee7490044f8f5bfbce34fb69fa4b
Crawcraw <crawcraw@crau.at.home>
2025-01-26 23:45:44 +0100

    final commit: implement generate pages recursively

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from webgen.fs import copy_files
-from webgen.gen import generate_page
+from webgen.gen import generate_pages_recursively
 
 
 def main():
     copy_files("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursively("content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
--- src/webgen/gen.py
+++ src/webgen/gen.py
@@ -28,3 +28,16 @@ def generate_page(from_path, template_path, dest_path):
     
     with open(dest_path, "w") as f:
         f.write(html_content)
+
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path, path=""):
+    dest_dir = os.path.join(dest_dir_path, path)
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+
+    with os.scandir(os.path.join(dir_path_content, path)) as it:
+        for entry in it:
+            if entry.is_dir():
+                generate_pages_recursively(dir_path_content, template_path, dest_dir_path, os.path.join(path, entry.name))
+            elif entry.is_file():
+                new_file = entry.name.rsplit(".", maxsplit=1)[0] + ".html"
+                generate_page(os.path.join(dir_path_content, path, entry.name), template_path, os.path.join(dest_dir, new_file))
